85dbbcddcd3d19f14d273c6fd8e33ed0ade668ba
jxndvs <jxn.dvs13@gmail.com>
2026-02-25 09:52:21 -0600

    Refactor User class to use ArrayList and constructor

--- User.java
+++ User.java
@@ -1,10 +1,18 @@
 package movie_rental_system;
 
+import java.util.ArrayList;
+
 public class User {
-	int id;
+	String id;
 	String name;
 	String password;
 	String checkedOut;
-	String[] history;
+	ArrayList<String> history;
 	float credit;
+	
+	User(String id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
 }
