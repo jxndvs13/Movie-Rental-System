66d7e895272729ae499327ac86efd7e0d8329eb9
jxndvs <jxn.dvs13@gmail.com>
2026-03-04 09:50:51 -0600

    Refactor Catalogue to use MySQL database
    Updated Catalogue class to connect to a MySQL database and perform queries.

--- Catalogue.java
+++ Catalogue.java
@@ -1,30 +1,41 @@
 package movie_rental_system;
 
-import java.util.HashMap;
+import java.sql.*;
 
 public class Catalogue {
-	static int addMov (HashMap<String, Movie> list, Movie mov, int id) {
-		String identifier;
-		if (id<10) {
-			identifier = "M00" + id;
-		}
-		else if (id<100) {
-			identifier = "M0" + id;
-		}
-		else {
-			identifier = "M" + id;
-		}
-		list.put(identifier, mov);
-		id += 1;
-		return id;
-	}
-	public static void main (String[] args) {
-		var database = new HashMap<String, Movie>();
-		int movId = database.size()+1;
-		
-		User person = new User("u01", "Steve", "Password!");
-		Movie film = new Movie("Star Wars", 1977, "Space", "George Lucas", "Lucasfilm", 3, 3.50);
-		movId = addMov(database, film, movId);
-		System.out.print(database.get("M001").title);
-	}
+	static final String DB_URL = "jdbc:mysql://localhost:3306/Movie_DB";
+	static final String USER = "root";
+	static final String PASS = "";
+	static final String QUERY = "SELECT id, title, year, stock FROM movies";
+	static final String UPDATE_QUERY = "UPDATE movies set stock=1 WHERE id=1";
+	   
+	public static void main(String[] args) {
+	      // Open a connection
+	      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	         Statement stmt = conn.createStatement();
+	         ) {
+	         // Let us check if it returns a true Result Set or not.
+	         Boolean ret = stmt.execute(UPDATE_QUERY);
+	         System.out.println("Return value is : " + ret.toString() );
+
+	         // Let us update age of the record with ID = 103;
+	         int rows = stmt.executeUpdate(UPDATE_QUERY);
+	         System.out.println("Rows impacted : " + rows );
+
+	         // Let us select all the records and display them.
+	         ResultSet rs = stmt.executeQuery(QUERY);		      
+
+	         // Extract data from result set
+	         while (rs.next()) {
+	            // Retrieve by column name
+	            System.out.print("ID: " + rs.getInt("id"));
+	            System.out.print(", Title: " + rs.getString("title"));
+	            System.out.print(", Year: " + rs.getInt("year"));
+	            System.out.println(", In Stock: " + rs.getInt("stock"));
+	         }
+	         rs.close();
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	      } 
+	   }
 }
