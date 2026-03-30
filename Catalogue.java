1bbf9fc59e0ab3e09225ff406c45a659b6946b72
jxndvs <jxn.dvs13@gmail.com>
2026-03-30 09:51:01 -0500

    Update Catalogue.java

--- Catalogue.java
+++ Catalogue.java
@@ -1,56 +1,58 @@
 package movie_rental_system;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Catalogue {
 	static final String DB_URL = "jdbc:mysql://localhost:3306/Movie_DB";
 	static final String USER = "root";
 	static final String PASS = "";
-	static final String Select = "SELECT id, title, year, stock FROM movies";
-	static final String Stock_d = "UPDATE movies set stock=1 WHERE id=1";
-	static final String Delete = "DELETE FROM movies WHERE id=3";
-	   
+	static Scanner scnr = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// Open a connection
-	      	try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-	      		Statement stmt = conn.createStatement();
-	      	) {
-	      		// Let us check if it returns a true Result Set or not.
-	      		Boolean ret = stmt.execute(Stock_d);
-	      		System.out.println("Return value is : " + ret.toString() );
-
-	      		// Let us update age of the record with ID = 103;
-	      		int rows = stmt.executeUpdate(Delete);
-	      		System.out.println("Rows impacted : " + rows );
-
-	      		// Let us select all the records and display them.
-	      		ResultSet rs = stmt.executeQuery(Select);		      
-
-	      		// Extract data from result set
-	      		while (rs.next()) {
-	      			// Retrieve by column name
+		viewCat();
+		int fid = scnr.nextInt();
+		findMov(fid);
+	}
+	
+	static void viewCat() {
+		String Select = "SELECT id, title, year, stock FROM movies";
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	            PreparedStatement pstmt = conn.prepareStatement(Select)) {
+				
+	            ResultSet rs = pstmt.executeQuery();
+	            
+	            while (rs.next()) {
 	      			System.out.print("ID: " + rs.getInt("id"));
 	      			System.out.print(", Title: " + rs.getString("title"));
 	      			System.out.print(", Year: " + rs.getInt("year"));
 	      			System.out.println(", In Stock: " + rs.getInt("stock"));
 	      		}
 	      		rs.close();
-	      } catch (SQLException e) {
-	    	  	e.printStackTrace();
-	      } 
+	            // last exception
+	    } catch (SQLException e) {
+	            e.printStackTrace();
+	    }
 	}
 	
-	static void printCatalogue(ResultSet rs) {
-		try {
-			while (rs.next()) {
-				System.out.print("ID: " + rs.getInt("id"));
-				System.out.print(", Title: " + rs.getString("title"));
-				System.out.print(", Year: " + rs.getInt("year"));
-				System.out.println(", In Stock: " + rs.getInt("stock"));
-			}
-		} catch (SQLException e) {
-    	  	e.printStackTrace();
-		} 
-		
+	static void findMov(int fid) {
+		String Find = "SELECT id, title, year, stock FROM movies WHERE id = ?";
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	            PreparedStatement pstmt = conn.prepareStatement(Find)) {
+
+				pstmt.setFloat(1, fid);
+	            ResultSet rs = pstmt.executeQuery();
+	            
+	            while (rs.next()) {
+	      			System.out.print("ID: " + rs.getInt("id"));
+	      			System.out.print(", Title: " + rs.getString("title"));
+	      			System.out.print(", Year: " + rs.getInt("year"));
+	      			System.out.println(", In Stock: " + rs.getInt("stock"));
+	      		}
+	      		rs.close();
+	            // last exception
+	    } catch (SQLException e) {
+	            e.printStackTrace();
+	    }
 	}
 }
